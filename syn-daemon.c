36f4f2d10f9ddffa9597fd74b885ba0465e48919
spinfoo <spinfoo.vuln@gmail.com>
2017-02-27 00:12:53 +0100

    tabify done

--- syn-daemon.c
+++ syn-daemon.c
@@ -31,20 +31,20 @@ int total=0,i,j;
 
 void print_tcp_packet(const u_char *buf, int s) {
 	struct iphdr *iph = (struct iphdr *)(buf  + sizeof(struct ethhdr) );
-    unsigned short iphdrlen= iph->ihl*4;
+	unsigned short iphdrlen= iph->ihl*4;
 	struct tcphdr *tcph=(struct tcphdr*)(buf + iphdrlen + sizeof(struct ethhdr));
-    int seq= ntohl(tcph->seq);
-    unsigned char bytes[4];
+	int seq= ntohl(tcph->seq);
+	unsigned char bytes[4];
 
-    bytes[0] = (seq >> 24) & 0xFF;
-    bytes[1] = (seq >> 16) & 0xFF;
-    bytes[2] = (seq >> 8) & 0xFF;
-    bytes[3] = seq & 0xFF;
+	bytes[0] = (seq >> 24) & 0xFF;
+	bytes[1] = (seq >> 16) & 0xFF;
+	bytes[2] = (seq >> 8) & 0xFF;
+	bytes[3] = seq & 0xFF;
 
-    fprintf(logfile, "%c%c%c%c", bytes[0], bytes[1], bytes[2], bytes[3]);
-    fflush(logfile);
-	fprintf(stderr, "[SYN=%d] ", tcph->syn);
-    fprintf(stderr, "[SEQ #= %x]\n", seq);
+	fprintf(logfile, "%c%c%c%c", bytes[0], bytes[1], bytes[2], bytes[3]);
+	fflush(logfile);
+	fprintf(stderr, "[SYN: %d] ", tcph->syn);
+	fprintf(stderr, "[SEQ #: 0x%x]\n", seq);
 }
 
 void process_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *buffer) {
@@ -60,51 +60,51 @@ void process_packet(u_char *args, const struct pcap_pkthdr *header, const u_char
 }
 
 void usage(char *argv) {
-    fprintf(stderr, "usage: %s -i iface -s source_ip -f file\n", argv);
+	fprintf(stderr, "usage: %s -i iface -s source_ip -f file\n", argv);
 }
 
 int main(int argc, char **argv) {
 	struct bpf_program fp;
 	bpf_u_int32 netp, maskp;
-    char rule[128];
+	char rule[128];
 	pcap_t *handle;
 	char errbuf[PCAP_ERRBUF_SIZE], devname[64], flog[128];
-    int c;
-    
-    rule[0]= '\0';
-    devname[0]= '\0';
-    flog[0]= '\0';
-    
-    if (argc != 7) {
-        usage(argv[0]);
-        exit(EXIT_FAILURE);
-    }
-    
-    while ((c = getopt(argc, argv, "i:s:f:")) != EOF) {
+	int c;
+	
+	rule[0]= '\0';
+	devname[0]= '\0';
+	flog[0]= '\0';
+	
+	if (argc != 7) {
+		usage(argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	
+	while ((c = getopt(argc, argv, "i:s:f:")) != EOF) {
 		switch (c) {
 			case 'i':
-                strncpy(devname, optarg, sizeof(devname)-1);
-                fprintf(stderr, "using interface: %s\n", devname);
+				strncpy(devname, optarg, sizeof(devname)-1);
+				fprintf(stderr, "using interface: %s\n", devname);
 				break;
 			case 's':
-                snprintf(rule, sizeof(rule)-1, "src host %s", optarg);
-                fprintf(stderr, "libcap rule: \"%s\"\n", rule);
+				snprintf(rule, sizeof(rule)-1, "src host %s", optarg);
+				fprintf(stderr, "libcap rule: \"%s\"\n", rule);
 				break;
 			case 'f':
-                strncpy(flog, optarg, sizeof(flog)-1);
+				strncpy(flog, optarg, sizeof(flog)-1);
 				break;
 			default:
-                usage(argv[0]);
+				usage(argv[0]);
 				exit(EXIT_FAILURE);
 		}
 	}
-    
-    if (*devname == '\0' || *rule == '\0' || *flog == '\0') {
-        usage(argv[0]);
-        exit(EXIT_FAILURE);
-    }
+	
+	if (*devname == '\0' || *rule == '\0' || *flog == '\0') {
+		usage(argv[0]);
+		exit(EXIT_FAILURE);
+	}
 
-    pcap_lookupnet(devname, &netp, &maskp, errbuf); 
+	pcap_lookupnet(devname, &netp, &maskp, errbuf); 
 
 	handle = pcap_open_live(devname, 65536, 1, 0, errbuf);
 	if (handle == NULL) {
@@ -115,7 +115,7 @@ int main(int argc, char **argv) {
 	logfile=fopen(flog,"w");
 	if(logfile == NULL)  {
 		printf("Unable to create file.");
-        exit(EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 
 	if(pcap_compile(handle, &fp, rule, 0, netp) == -1) {
